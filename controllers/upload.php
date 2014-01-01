cd8af73499b13dbd1ecd77bc28b1e0f2f42bec44
Adam <adam.gedney@gmail.com>
2014-01-01 16:16:00 -0500

    test

--- controllers/upload.php
+++ controllers/upload.php
@@ -83,7 +83,7 @@ if(isset($_FILES['file'])){
 
 		//adds video & still paths to database
 		$model->add_Video($webm, $mp4, $mov, $ogv, $flv, $mp3, $shot1, $shot2, $shot3, $poster, $title);
-		echo "success";
+		var_dump($first_process);
 		//strips actions from URL by reloading site after complete conversions
 		// if($last_process){
 		// 	// var_dump($last_process);
